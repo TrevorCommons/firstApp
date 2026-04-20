be3fe08e1c8e37a349f09680eddb5ac84b6a7027
TrevorCommons <commonstrevor@gmail.com>
2026-04-19 20:57:34 -0400

    creating desktop app first attempt, learning as i go

---
+++ src/components/profile.jsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <img
+      src="https://slaythespire.wiki.gg/images/thumb/StS2_Ironclad-Defend.png/150px-StS2_Ironclad-Defend.png?f8f7e4"
+      alt="Ironclad Defend Card"
+    />
+  )
+}
